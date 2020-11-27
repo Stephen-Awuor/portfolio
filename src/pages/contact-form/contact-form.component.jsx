180eb511e6949ad1a5949212c8f48d134ea53cbf
SteveKaneya <stevekaneya@gmail.com>
2020-11-27 13:10:10 +0300

    changes on links

--- src/pages/contact-form/contact-form.component.jsx
+++ src/pages/contact-form/contact-form.component.jsx
@@ -28,7 +28,7 @@ const ContactForm = () => {
             </div>
             
             <div className="m-2">
-              <a href="https://github.com/SteveKaneya/SteveKaneya" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/SteveKaneya/" target="_blank" rel="noopener noreferrer">
                 <Button variant="outline-dark" title="My other projects">
                   <i className="fab fa-github-square"></i> GitHub
                 </Button>
